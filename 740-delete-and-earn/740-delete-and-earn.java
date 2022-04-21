07e060038586cee56278c197d508a2a0796b9974
marmik2001 <58849174+marmik2001@users.noreply.github.com>
2022-04-21 14:51:23 +0530

    Time: 4 ms (80.80%), Space: 47.3 MB (25.98%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -2,11 +2,13 @@ class Solution {
     public int deleteAndEarn(int[] nums) {
         int[] arr = new int[10001];
         for(int i:nums)
-            arr[i]+=i;
-        
+            arr[i-1]++;
+        int p1 = arr[0],p2 = Math.max(arr[0],arr[1]*2);
         for(int i=2;i<=10000;i++){
-            arr[i] = Math.max(arr[i-1],arr[i-2]+arr[i]);
+            int x = Math.max(p1+arr[i]*(i+1),p2);
+            p1 = p2;
+            p2 = x;
         }
-        return arr[10000];
+        return p2;
     }
 }
