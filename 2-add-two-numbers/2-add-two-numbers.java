b0f24c0c0abf4c02347b250e91f0e560f842a969
marmik2001 <58849174+marmik2001@users.noreply.github.com>
2022-05-15 12:30:15 +0530

    Time: 2 ms (90.06%), Space: 47.8 MB (45.52%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,62 +10,22 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry = 0;
-        ListNode head = new ListNode(), prev = null;
-        ListNode temp = head;
-        while (l1 != null & l2 != null) {
-            temp.val = (carry + l1.val + l2.val) % 10;
-            carry = (carry + l1.val + l2.val) / 10;
-            temp.next = new ListNode();
-            prev = temp;
-            temp = temp.next;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        if (l1 != null){
-            while (carry != 0 && l1 != null) {
-                temp.val = (carry + l1.val) % 10;
-                carry = (carry + l1.val) / 10;
-                temp.next = new ListNode();
-                prev = temp;
-                temp = temp.next;
-                l1 = l1.next;
-            }
-            if(l1!=null){
-                prev.next = l1;
-                return head;
-            }
-        }
-        if (l2 != null){
-            while (carry != 0 && l2 != null) {
-                temp.val = (carry + l2.val) % 10;
-                carry = (carry + l2.val) / 10;
-                temp.next = new ListNode();
-                prev = temp;
-                temp = temp.next;
-                l2 = l2.next;
-            }
-            if(l2!=null){
-                prev.next = l2;
-                return head;
-            }
-        }
-        if (carry != 0) {
-            temp.val = carry;
-            return head;
-        }
-        prev.next = null;
-        return head;
+    ListNode dummyHead = new ListNode(0);
+    ListNode p = l1, q = l2, curr = dummyHead;
+    int carry = 0;
+    while (p != null || q != null) {
+        int x = (p != null) ? p.val : 0;
+        int y = (q != null) ? q.val : 0;
+        int sum = carry + x + y;
+        carry = sum / 10;
+        curr.next = new ListNode(sum % 10);
+        curr = curr.next;
+        if (p != null) p = p.next;
+        if (q != null) q = q.next;
     }
-    
+    if (carry > 0) {
+        curr.next = new ListNode(carry);
+    }
+    return dummyHead.next;
 }
-
-
-
-
-
-
-
-
-
-
+}
