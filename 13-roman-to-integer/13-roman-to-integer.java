87efcf4d914ae100a80ad3a62ee59dbedfc45104
marmik2001 <58849174+marmik2001@users.noreply.github.com>
2022-06-20 17:07:49 +0530

    Time: 3 ms (99.98%), Space: 42 MB (98.90%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int romanToInt(String s) {
+        int n = s.length();
+        int[] nums = new int[n];
+        for(int i=0;i<n;i++){
+            switch(s.charAt(i)){
+                case 'I' : nums[i] = 1;break;
+                case 'V' : nums[i] = 5;break;
+                case 'X' : nums[i] = 10;break;
+                case 'L' : nums[i] = 50;break;
+                case 'C' : nums[i] = 100;break;
+                case 'D' : nums[i] = 500;break;
+                case 'M' : nums[i] = 1000;break;
+                case default : break;
+            }
+        }
+        int sum = 0;
+        for(int i=0;i<n-1;i++){
+            if(nums[i+1]>nums[i])sum-=nums[i];
+            else sum+=nums[i];
+        }
+        return sum+nums[n-1];
+    }
+}
