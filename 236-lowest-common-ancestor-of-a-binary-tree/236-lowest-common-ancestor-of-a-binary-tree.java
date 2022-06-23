1ee45366c60c54a3917677ea923b447a9899877a
marmik2001 <58849174+marmik2001@users.noreply.github.com>
2022-06-23 16:00:05 +0530

    Time: 11 ms (44.76%), Space: 47.8 MB (21.86%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,32 +1,3 @@
-class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> p1 = path(root,p.val);
-        ArrayList<TreeNode> p2 = path(root,q.val);
-        int limit = Math.min(p1.size(),p2.size())-1;
-        for(int i=0;i<limit;i++){
-            if(p1.get(i+1)!=p2.get(i+1))
-                return p1.get(i);
-        }
-        return p1.get(limit);
-    }
-    public ArrayList<TreeNode> path(TreeNode A, int B) {
-        ArrayList<TreeNode> sol = new ArrayList<>();
-        found(A,B,sol);
-        return sol;
-    }
-    private boolean found(TreeNode root, int B, ArrayList<TreeNode> sol){
-        if(root==null)return false;
-        if(root.val==B){
-            sol.add(root);
-            return true;
-        }
-        sol.add(root);
-        if(found(root.left,B,sol))return true;
-        if(found(root.right,B,sol))return true;
-        sol.remove(sol.size()-1);
-        return false;
-    }
-}
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -36,3 +7,11 @@ class Solution {
  *     TreeNode(int x) { val = x; }
  * }
  */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null||root==p||root==q)return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        return left==null?right:(right==null?left:root);  
+    }
+}
