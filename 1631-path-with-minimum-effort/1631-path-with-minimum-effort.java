883e08b305d438265c522c0878c0f81dcf8f5935
marmik2001 <58849174+marmik2001@users.noreply.github.com>
2022-07-09 11:43:51 +0530

    Time: 98 ms (49.61%), Space: 57.6 MB (67.83%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,20 +1,23 @@
 class Solution {
     public int minimumEffortPath(int[][] heights) {
         int m = heights.length, n = heights[0].length;
-        int[][] vector = new int[][]{{0,1},{0,-1},{-1,0},{1,0}}; 
-        boolean[][] done = new boolean[m][n];
+        int[][] vector = new int[][]{{0,1},{0,-1},{-1,0},{1,0}};
+        int[][] dist = new int[m][n];
+        for(int[] x:dist)
+            Arrays.fill(x,Integer.MAX_VALUE);
+        dist[0][0] = 0;
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o->o[2]));
         pq.add(new int[]{0,0,0});
         while(!pq.isEmpty()){
             int[] x = pq.poll();
-            if(done[x[0]][x[1]])continue;
-            done[x[0]][x[1]] = true;
+            if(x[2]>dist[x[0]][x[1]])continue;
             if(x[0]==m-1&&x[1]==n-1)return x[2];
             for(int[] v:vector){
                 int r = x[0]+v[0];
                 int c = x[1]+v[1];
-                if(r<0||r==m||c<0||c==n||done[r][c])continue;
-                pq.add(new int[]{r,c,Math.max(x[2],Math.abs(heights[x[0]][x[1]]-heights[r][c]))});
+                if(r<0||r==m||c<0||c==n||dist[r][c]<=Math.max(x[2],Math.abs(heights[x[0]][x[1]]-heights[r][c])))continue;
+                dist[r][c] = Math.max(x[2],Math.abs(heights[x[0]][x[1]]-heights[r][c]));
+                pq.add(new int[]{r,c,dist[r][c]});
             }
         }
         return -1;
