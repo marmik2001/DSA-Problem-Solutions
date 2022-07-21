dd0202def6e8e2b28d545b64443cceb84491fec5
marmik2001 <58849174+marmik2001@users.noreply.github.com>
2022-07-21 17:09:07 +0530

    Time: 1 ms (66.05%), Space: 42 MB (59.08%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,27 +14,16 @@
  * }
  */
 class Solution {
-    public TreeNode bstFromPreorder(int[] preorder) {
-        int n = preorder.length;
-        int[] nextLargerIndex = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        for(int i=n-1;i>=0;i--){
-            while(!stack.isEmpty()&&preorder[stack.peek()]<preorder[i])
-                stack.pop();
-            if(stack.isEmpty())
-                nextLargerIndex[i] = n;
-            else
-                nextLargerIndex[i] = stack.peek();
-            stack.add(i);
-        }
-        return helper(preorder,nextLargerIndex,0,n-1);
+    int i = 0;
+    public TreeNode bstFromPreorder(int[] A) {
+        return bstFromPreorder(A, Integer.MAX_VALUE);
     }
-    private TreeNode helper(int[] preorder, int[] nextLargerIndex, int i, int j){
-        if(j<i)return null;
-        TreeNode x = new TreeNode(preorder[i]);
-        if(i==j)return x;
-        x.left = helper(preorder, nextLargerIndex, i+1, nextLargerIndex[i]-1);
-        x.right = helper(preorder, nextLargerIndex, nextLargerIndex[i], j);
-        return x;
+
+    public TreeNode bstFromPreorder(int[] A, int bound) {
+        if (i == A.length || A[i] > bound) return null;
+        TreeNode root = new TreeNode(A[i++]);
+        root.left = bstFromPreorder(A, root.val);
+        root.right = bstFromPreorder(A, bound);
+        return root;
     }
 }
